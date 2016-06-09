bd828b38691ce7955074e31f30cd0a32c3210353
rajkumar <rajkumar.gootam@gmail.com>
2016-06-09 18:51:56 +0530

    Added local CDN files index.html

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 var angularApp = angular.module('governmentApp', ['ngResource','ngRoute']);
+
+
+
+
+
+
+
+
+
+
+
+
+angularApp.controller("mainController",function($resource){
+  var vm=this;
+  vm.getInfo = function(){
+    console.log(vm.cityName);
+    var govResource = $resource('http://api.openweathermap.org/data/2.5/weather',
+      {q:vm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa'});
+    vm.weather = weatherResource.get();
+   }
+});
