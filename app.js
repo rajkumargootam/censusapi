fbe80534e0b99aab9c3d42877718444c1826c4c5
rajkumar <rajkumar.gootam@gmail.com>
2016-06-09 23:04:38 +0530

    added second controller in app.js and stylishing app

--- app.js
+++ app.js
@@ -2,23 +2,40 @@ var angularApp = angular.module('governmentApp', ['ngResource','ngRoute']);
 
 angularApp.config(function ($routeProvider){
   $routeProvider
+
   .when ('/',
   {
     templateUrl: 'list/main.html',
     controller: 'GovernmentController',
     controllerAs: 'hm'
   })
+
+  .when ('/percent',
+  {
+    templateUrl: 'list/percentage.html',
+    controller: 'PercentController',
+    controllerAs: 'pc'
+  })
   });
 
 
 angularApp.controller("GovernmentController",['$resource','$filter',function($resource,$filter){
   var vm=this;
-
-
     vm.getInfo = function(){
       console.log(this)
       var govtResource = $resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP&SEX=0');
       vm.govtResponse = govtResource.query({FIPS:vm.country,time:vm.year,key:'099b6855b62209b10274a5d6cec26395c298f8eb'});
       console.log(vm.govtResponse);
     }
+}]);
+
+angularApp.controller("PercentController",['$resource','$filter',function($resource,$filter){
+  var vm=this;
+      var percentResource = $resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP&SEX=0');
+      vm.percentResponse = percentResource.query({FIPS:vm.country,time:vm.year,key:'099b6855b62209b10274a5d6cec26395c298f8eb'});
+      console.log(vm.percentResponse);
+
+      vm.getpercent = function(){
+        console.log(this);
+      }
 }]);
