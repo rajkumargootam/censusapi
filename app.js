b819d2c24371ffad07f4e51597be92e1692f6d49
rajkumar <rajkumar.gootam@gmail.com>
2016-06-11 11:52:40 +0530

    added button to main page and percent page

--- app.js
+++ app.js
@@ -31,11 +31,14 @@ angularApp.controller("GovernmentController",['$resource','$filter',function($re
 
 angularApp.controller("PercentController",['$resource','$filter',function($resource,$filter){
   var vm=this;
-      var percentResource = $resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP&SEX=0');
-      vm.percentResponse = percentResource.query({FIPS:vm.country,time:vm.year,key:'099b6855b62209b10274a5d6cec26395c298f8eb'});
-      console.log(vm.percentResponse);
 
       vm.getpercent = function(){
-        console.log(this);
+        console.log("hello");
+        var percentResource = $resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP&SEX=0');
+        vm.percentResponse = percentResource.query({FIPS:vm.country,time:vm.year,key:'099b6855b62209b10274a5d6cec26395c298f8eb'});
+        console.log(vm.country);
+        console.log(vm.percentResponse);
       }
+
+
 }]);
